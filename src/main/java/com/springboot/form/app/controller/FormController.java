3059479b5573b2c5ad55c0a534b0980a33f66674
ivan sebastian <inunez@banksa.com.ar>
2023-04-17 15:55:52 -0300

    Validaciones con Java

--- src/main/java/com/springboot/form/app/controller/FormController.java
+++ src/main/java/com/springboot/form/app/controller/FormController.java
@@ -23,41 +23,37 @@ public class FormController {
 		return "form";
 	}
 
-
 	@PostMapping("/form")
-	public String procesar(@Valid Usuario usuario,BindingResult result, Model model) {
+	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
 
 		model.addAttribute("titulo", "Resultado del formulario");
 
-//		if(result.hasErrors()) {
-//			Map<String, String> errores = new HashMap<>();
-//			result.getFieldErrors().forEach(err -> {
-//				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
-//			});
-//			model.addAttribute("error", errores);
-//			return "form";
-//		}
+		if (result.hasErrors()) {
+			Map<String, String> errores = new HashMap<>();
+			result.getFieldErrors().forEach(err -> {
+				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
+			});
+			model.addAttribute("error", errores);
+			return "form";
+		}
+		
 		model.addAttribute("usuario", usuario);
 
 		return "resultado";
 	}
-	
-	
-	
-	
-/*	@PostMapping("/form")
-	public String procesar(Model model, @RequestParam String username, @RequestParam String password, @RequestParam String email) {
 
-		Usuario usuario = new Usuario(); 
-		
-		usuario.setUsername(username);
-		usuario.setPassword(password);
-		usuario.setEmail(email);
-		
-		model.addAttribute("titulo", "Resultado del formulario");
-		model.addAttribute("usuario", usuario);
-		
-		return "resultado";
-	}
-*/
+	/*
+	 * @PostMapping("/form") public String procesar(Model model, @RequestParam
+	 * String username, @RequestParam String password, @RequestParam String email) {
+	 * 
+	 * Usuario usuario = new Usuario();
+	 * 
+	 * usuario.setUsername(username); usuario.setPassword(password);
+	 * usuario.setEmail(email);
+	 * 
+	 * model.addAttribute("titulo", "Resultado del formulario");
+	 * model.addAttribute("usuario", usuario);
+	 * 
+	 * return "resultado"; }
+	 */
 }
