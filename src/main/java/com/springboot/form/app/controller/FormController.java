51250adcbec88b7cb5549892f5993cfde376f481
ivan sebastian <inunez@banksa.com.ar>
2023-04-18 14:53:51 -0300

    Validaciones de los imputs por parte de th

--- src/main/java/com/springboot/form/app/controller/FormController.java
+++ src/main/java/com/springboot/form/app/controller/FormController.java
@@ -33,13 +33,17 @@ public class FormController {
 		model.addAttribute("titulo", "Resultado del formulario");
 
 		if (result.hasErrors()) {
-			Map<String, String> errores = new HashMap<>();
-			result.getFieldErrors().forEach(err -> {
-				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
-			});
-			model.addAttribute("error", errores);
 			return "form";
 		}
+// En vez de captar los errores con Java lo captamos por thymeleaf		
+//		if (result.hasErrors()) {
+//			Map<String, String> errores = new HashMap<>();
+//			result.getFieldErrors().forEach(err -> {
+//				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
+//			});
+//			model.addAttribute("error", errores);
+//			return "form";
+//		}
 		
 		model.addAttribute("usuario", usuario);
 
